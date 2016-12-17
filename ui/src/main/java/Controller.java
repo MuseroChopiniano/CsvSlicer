9b2cc193de0e4a53c9b4a9f2dc910073d662914a
FacundoTripelhorn <facundo.tripelhorn@gmail.com>
2016-12-17 20:28:48 -0300

    Vista y controller

--- ui/src/main/java/Controller.java
+++ ui/src/main/java/Controller.java
@@ -1,7 +1,55 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by GastónAlejandro on 17/12/2016.
  */
 public class Controller {
     /**TODO: Definir toda la clase Controller*/
+    private DefaultTableModel modelo;
+    private Archivo archivo;
 
+    public DefaultTableModel leerArchivo() throws FileNotFoundException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        modelo=new DefaultTableModel();
+        Lector lector= (Lector) context.getBean("lector");
+        archivo = lector.leerArchivo(seleccionarArchivo());
+        cargarTabla(archivo);
+        return modelo;
+    }
+
+    private void cargarTabla(Archivo archivo){
+        for (int i = 0; i < archivo.getListaDeColumnas().size(); i++) {
+            modelo.addColumn(archivo.getListaDeColumnas().get(i).getNombre());
+        }
+        for (int j = 0; j < archivo.getListaDeFilas().size(); j++) {
+            List<Object> celdas = new ArrayList<Object>();
+            for (int k = 0; k < archivo.getListaDeFilas().get(j).getListaDeCeldas().size(); k++) {
+                celdas.add(archivo.getListaDeFilas().get(j).getListaDeCeldas().get(k).getValor());
+            }
+            Object[] fila = new Object[celdas.size()];
+            for (int i = 0; i < celdas.size(); i++) {
+                fila[i] = celdas.get(i);
+            }
+            modelo.addRow(fila);
+        }
+    }
+
+    private String seleccionarArchivo(){
+        File selectedFile=null;
+        JFileChooser fileChooser=new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = fileChooser.showOpenDialog(new JPanel());
+        if (result==JFileChooser.APPROVE_OPTION) {
+            selectedFile = fileChooser.getSelectedFile();
+        }
+        return selectedFile.getPath();
+    }
 }
--- ui/src/main/java/Vista.java
+++ ui/src/main/java/Vista.java
@@ -17,13 +17,12 @@ import java.util.List;
  */
 public class Vista extends JFrame implements ActionListener{
     private JButton button1;
-    private JTextField textField1;
-    private JTextField textField2;
     private JTable table1;
     private JPanel panel1;
-    private JComboBox comboBox1;
     private JButton removerColumnaButton;
-    private DefaultTableModel modelo;
+    private JList list1;
+    private JTextField cantidadDeFilasTextField;
+private Controller controlador = new Controller();
 
     public Vista() {
         button1.addActionListener(this);
@@ -41,7 +40,9 @@ public class Vista extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if (e.getSource()==button1){
             try {
-                leerArchivo();
+               table1.setModel(controlador.leerArchivo());
+                cargarLista();
+                cantidadDeFilasTextField.setText(String.valueOf(table1.getRowCount()));
             } catch (FileNotFoundException e1) {
                 e1.printStackTrace();
             }
@@ -50,53 +51,21 @@ public class Vista extends JFrame implements ActionListener{
         }
     }
 
-    public void leerArchivo() throws FileNotFoundException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        modelo=new DefaultTableModel();
-        Archivo archivo ;
-        Lector lector= (Lector) context.getBean("lector");
-        archivo = lector.leerArchivo(seleccionarArchivo());
-        for (int i=0;i<archivo.getListaDeColumnas().size();i++){
-            comboBox1.addItem(archivo.getListaDeColumnas().get(i).getNombre());
-        }
-        cargarTabla(archivo);
-        table1.setModel(modelo);
-    }
-
-    private void cargarTabla(Archivo archivo){
-        for (int i = 0; i < archivo.getListaDeColumnas().size(); i++) {
-            modelo.addColumn(archivo.getListaDeColumnas().get(i).getNombre());
-        }
-        for (int j = 0; j < archivo.getListaDeFilas().size(); j++) {
-            List<Object> celdas = new ArrayList<Object>();
-            for (int k = 0; k < archivo.getListaDeFilas().get(j).getListaDeCeldas().size(); k++) {
-                celdas.add(archivo.getListaDeFilas().get(j).getListaDeCeldas().get(k).getValor());
-            }
-            Object[] fila = new Object[celdas.size()];
-            for (int i = 0; i < celdas.size(); i++) {
-                fila[i] = celdas.get(i);
-            }
-            modelo.addRow(fila);
-        }
-    }
-
-    private String seleccionarArchivo(){
-        File selectedFile=null;
-        JFileChooser fileChooser=new JFileChooser();
-        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-        int result = fileChooser.showOpenDialog(new JPanel());
-        if (result==JFileChooser.APPROVE_OPTION) {
-             selectedFile = fileChooser.getSelectedFile();
-        }
-        return selectedFile.getPath();
-    }
-
     private void removerColumna(){
         for(int i=0;i<table1.getColumnCount();i++){
-            if(table1.getColumnName(i)==comboBox1.getSelectedItem().toString()){
+            if(table1.getColumnName(i)==list1.getSelectedValue().toString()){
                 TableColumn column=table1.getColumnModel().getColumn(i);
                 table1.removeColumn(column);
+                cargarLista();
             }
         }
+    }
+
+    private void cargarLista(){
+        DefaultListModel listModel=new DefaultListModel();
+        for (int i=0;i<table1.getColumnCount();i++){
+            listModel.addElement(table1.getColumnName(i));
+        }
+        list1.setModel(listModel);
     }
 }
